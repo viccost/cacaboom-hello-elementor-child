28fa5eb0439495bb625424ce7ac25ba7c0e546e9
Victor <victorcost_@outlook.com>
2023-02-28 09:50:04 -0300

    adding GTM tags

--- functions.php
+++ functions.php
@@ -39,9 +39,29 @@ function yikes_hide_tab_title(){
     add_filter( 'woocommerce_product_additional_information_heading', '__return_false', 99 );
 }
 
+function addGoogleanalytics() { ?>
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id=%27+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-TR4962Q');</script>
+	<!-- End Google Tag Manager -->
+<?php };
+
+function addGoogleanalyticsBody() { ?>
+	<!-- Google Tag Manager (noscript) -->
+	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TR4962Q"
+	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<!-- End Google Tag Manager (nooscript) -->
+<?php };
+
 add_action('wp_enqueue_scripts', 'my_pagseguro_css', 11);
 add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles', 11);
 add_action( 'init', 'yikes_hide_tab_title', 10 );
+add_action('wp_head', 'addGoogleanalytics');
+add_action('wp_body_open', 'addGoogleanalyticsBody');
+
 
 
 function my_woocommerce_product_tabs($tabs)
